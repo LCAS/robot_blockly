585b1ad4a8b8f86d95546b26f777199e2ed7a686
Marc Hanheide <marc@hanheide.net>
2017-06-26 14:08:52 +0100

    changed back to python2

--- scripts/robot_blockly_backend.py
+++ scripts/robot_blockly_backend.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 # Software License Agreement (BSD License)
 #
 # Copyright (c) 2015, Erle Robotics LLC
@@ -38,7 +38,6 @@ import os
 import threading
 import signal
 import rosnode
-import mavros
 
 from subprocess import Popen
 from std_msgs.msg import String
@@ -46,18 +45,7 @@ from std_srvs.srv import Empty, EmptyResponse, Trigger, TriggerResponse
 from autobahn.asyncio.websocket import WebSocketServerProtocol, \
     WebSocketServerFactory
 from robot_blockly.srv import SetCurrentBlockId, SetCurrentBlockIdResponse
-from std_msgs.msg import String
 from sensor_msgs.msg import Joy
-from crab_msgs.msg import apm_imu
-from crab_msgs.msg import BodyCommand
-from crab_msgs.msg import BodyState
-from crab_msgs.msg import GaitCommand
-from crab_msgs.msg import LegIKRequest
-from crab_msgs.msg import LegJointsState
-from crab_msgs.msg import LegPositionState
-from crab_msgs.msg import LegsJointsState
-from mavros_msgs.msg import OverrideRCIn
-from mavros_msgs.srv import SetMode
 
 try:
     import asyncio
