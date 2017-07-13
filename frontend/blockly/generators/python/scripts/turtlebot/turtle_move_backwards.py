29c22c3a3929c9883273c5a4dcb5b1e075da589b
paul-baxter <pebaxter@gmail.com>
2017-07-13 16:57:47 +0100

    updated block source for turtlebot moving

--- frontend/blockly/generators/python/scripts/turtlebot/turtle_move_backwards.py
+++ frontend/blockly/generators/python/scripts/turtlebot/turtle_move_backwards.py
@@ -2,8 +2,6 @@ import rospy
 import time
 from geometry_msgs.msg import Twist
 
-publisher = rospy.Publisher("/mobile_base/commands/velocity", Twist, queue_size=1)
-
 #maximum speed check!
 if vel > 2:
     vel = 2
