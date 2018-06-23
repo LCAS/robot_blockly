b298b0f86a98edc8407866f58c26519900673e98
Paul Baxter <pebaxter@gmail.com>
2018-06-23 03:13:55 +0100

    changed distance sensor block, and compressed files

--- frontend/blockly/generators/python/scripts/turtlebot/turtle_distance_middle.py
+++ frontend/blockly/generators/python/scripts/turtlebot/turtle_distance_middle.py
@@ -2,8 +2,10 @@ import numpy
 import rospy
 from sensor_msgs.msg import LaserScan
 
-distance_data = rospy.wait_for_message('/scan', LaserScan, timeout=2)
+distance_data = rospy.wait_for_message('/scan', LaserScan, timeout=5)
 
-distance_middle = numpy.nanmean(distance_data.ranges[300:340])
+distance_middle_temp = distance_data.ranges[300:340]
+
+distance_middle = numpy.nanmean(distance_middle_temp.astype('float64'))
 
 # use range 0:40 for left and 600:639 for right
