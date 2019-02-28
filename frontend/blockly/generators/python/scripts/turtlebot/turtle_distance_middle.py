9d81d48b0a560d0df86627dc17fa4057e1886397
Marc Hanheide <marc@hanheide.net>
2019-02-28 12:50:07 +0000

    changed to cover the entire scan area

--- frontend/blockly/generators/python/scripts/turtlebot/turtle_distance_middle.py
+++ frontend/blockly/generators/python/scripts/turtlebot/turtle_distance_middle.py
@@ -4,6 +4,6 @@ from sensor_msgs.msg import LaserScan
 
 distance_data = rospy.wait_for_message('/scan', LaserScan, timeout=5)
 
-distance_middle = numpy.nanmean(distance_data.ranges[300:340], dtype='float64')
+distance_middle = numpy.nanmean(distance_data.ranges[201:439], dtype='float64')
 
 # use range 0:40 for left and 600:639 for right
